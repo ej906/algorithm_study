f81b916880eda310b3f996883182048646e1cd10
ej906 <136413741+ej906@users.noreply.github.com>
2023-08-03 17:28:07 +0900

    [Silver I] Title: 물병, Time: 48 ms, Memory: 33324 KB -BaekjoonHub

--- 백준/Silver/1052. 물병/물병.py
+++ 백준/Silver/1052. 물병/물병.py
@@ -10,7 +10,8 @@ n,k=map(int,input().split())
 cnt=0
 
 while bin(n).count('1')>k: #2진수로 바꾸고 그중 1인것(컵)의 개수를 센다
-    n+=1
-    cnt+=1
-
+    idx=bin(n)[::-1].index('1')
+    cnt+=2**idx
+    n+=2**idx
+    
 print(cnt)
